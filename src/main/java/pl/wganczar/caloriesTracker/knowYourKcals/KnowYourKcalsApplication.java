00ddff7cb391f19b0bcbdfbf48ff2669c928af66
Wojciech Ganczar <woiciech5@gmail.com>
2025-07-31 13:44:38 +0200

    Init commit

---
+++ src/main/java/pl/wganczar/caloriesTracker/knowYourKcals/KnowYourKcalsApplication.java
@@ -0,0 +1,13 @@
+package pl.wganczar.caloriesTracker.knowYourKcals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowYourKcalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowYourKcalsApplication.class, args);
+	}
+
+}
